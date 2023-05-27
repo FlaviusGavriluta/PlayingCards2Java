74ebbd40ada90a231bcb3ca746aced9e507982b6
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-05-27 16:58:26 +0300

    turning the card class into a record

--- src/com/codecool/playingcards/model/Card.java
+++ src/com/codecool/playingcards/model/Card.java
@@ -1,46 +1,12 @@
 package com.codecool.playingcards.model;
 
-import java.util.Objects;
-
-public class Card {
-
-    private final String symbol;
-    private final String suit;
-    private final String title;
-
-    public Card(String symbol, String suit) {
-        this.symbol = symbol;
-        this.suit = suit;
-        this.title = this.symbol + " of " + suit;
-    }
-
-    public String getSymbol() {
-        return symbol;
-    }
-
-    public String getSuit() {
-        return suit;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Card card = (Card) o;
-        return suit.equals(card.suit) && symbol.equals(card.symbol);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(symbol, suit);
+public record Card(String symbol, String suit) {
+    public String title() {
+        return String.format("%s of %s", symbol, suit);
     }
 
     @Override
     public String toString() {
-        return title;
+        return title();
     }
 }
