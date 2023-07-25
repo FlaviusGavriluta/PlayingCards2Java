64d4aa8381643ca84e35c49cc970bb25b96d4312
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-25 16:44:40 +0300

    tested a drawOne when deck is not empty

--- src/test/java/com/service/impl/DeckTest.java
+++ src/test/java/com/service/impl/DeckTest.java
@@ -6,16 +6,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeckTest {
     private Deck emptyDeck;
+    private Deck deckWithCards;
 
     @BeforeEach
     void setUp() {
         emptyDeck = new Deck(new ArrayList<>());
+        deckWithCards = new Deck(List.of(new Card("3", "Spades"), new Card("7", "Hearts")));
     }
 
     @Test
@@ -29,7 +32,7 @@ public class DeckTest {
     @Test
     void testDrawOneWhenDeckIsNotEmpty() {
         // Act
-        Optional<Card> card = emptyDeck.drawOne();
+        Optional<Card> card = deckWithCards.drawOne();
         // Verificăm că rezultatul este Optional<Card> (adică că s-a extras o carte) când pachetul are cărți
         assertTrue(card.isPresent());
     }
