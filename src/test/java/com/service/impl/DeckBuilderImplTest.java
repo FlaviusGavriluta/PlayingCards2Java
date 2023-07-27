b4f3f50af1e5ff3a9a9f670baf3ca82a7d0c6b3b
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-27 14:13:22 +0300

    refactor test create deck returns deck with generated cards

--- src/test/java/com/service/impl/DeckBuilderImplTest.java
+++ src/test/java/com/service/impl/DeckBuilderImplTest.java
@@ -7,10 +7,14 @@ import com.service.CardGenerator;
 import com.service.DeckBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -27,14 +31,11 @@ class DeckBuilderImplTest {
         deckDescriptor = new DeckDescriptor(new int[0], new String[0], new String[0]); //DeckDescriptor contents doesn't matter - we just need to create a new DeckDescriptor object
     }
 
-    @Test
-    void createDeckReturnsNewDeckWithGeneratedCards() {
+    @ParameterizedTest
+    @MethodSource("provideDecksAndExpectedCardCounts")
+    void createDeckReturnsNewDeckWithGeneratedCards(List<Card> cards, int expectedCardCount) {
         // Arrange
-        List<Card> original = List.of(new Card("Ace", "Spades"), new Card("Ace", "Hearts"));
-        List<Card> cards = new ArrayList<>(original);
-
         when(cardGeneratorMock.generate(deckDescriptor)).thenReturn(cards); //When generate is called on the cardGeneratorMock, we return the cards list
-
         DeckBuilder deckBuilder = new DeckBuilderImpl(cardGeneratorMock, deckDescriptor);
 
         // Act
@@ -44,10 +45,19 @@ class DeckBuilderImplTest {
         // Assert
         Optional<Card> drawn = deck.drawOne();
         while (drawn.isPresent()) {
-            assertTrue(original.contains(drawn.get()));
+            assertTrue(cards.contains(drawn.get()));
             drawn = deck.drawOne();
         }
 
-        assertEquals(2, cardCount);
+        assertEquals(expectedCardCount, cardCount);
+    }
+
+    private static Stream<Arguments> provideDecksAndExpectedCardCounts() {
+        List<Card> deck1 = List.of(new Card("Ace", "Spades"), new Card("Ace", "Hearts"));
+        List<Card> deck2 = List.of(new Card("3", "Spades"), new Card("7", "Hearts"), new Card("A", "Diamonds"));
+        return Stream.of(
+                Arguments.of(deck1, 2),
+                Arguments.of(deck2, 3)
+        );
     }
 }
